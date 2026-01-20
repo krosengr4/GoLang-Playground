4c5d5e8ce7b3e5ab0125b77867ef64dcf21916ae
Kevin Rosengren <rosenkev4@gmail.com>
2026-01-20 02:30:07 -0600

    feat(exercises): add fizzbuzz exercise
    Classic fizzbuzz implementation using a switch statement.
    Prints integers 1-100, replacing multiples of 3 with "fizz",
    multiples of 5 with "buzz", and multiples of both with "fizzbuzz".

---
+++ fizzbuzz/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Good ol fizzbuzz
+// Print the integers 1 - 100
+// for multiples of 3, print fizz instead of the number
+// for multiples of 5, print buzz instead of the number
+// And for multiples of 3 and 5... you guessed it, print fizzbuzz instead of the number
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		switch {
+		case i % 15 == 0:
+			fmt.Println("fizzbuzz!")
+		case i % 5 == 0:
+			fmt.Println("buzz")
+		case i % 3 == 0:
+			fmt.Println("fizz")
+		default:
+			fmt.Println(i)
+		}
+	}
+}
