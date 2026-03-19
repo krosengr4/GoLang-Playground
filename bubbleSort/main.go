4d71806277c173ff43a5d30ede79cccc6025a949
Kevin Rosengren <rosenkev4@gmail.com>
2026-03-19 04:59:00 -0500

    Go saving tidy

--- bubbleSort/main.go
+++ bubbleSort/main.go
@@ -15,20 +15,19 @@ func main() {
 	fmt.Println(strings.Repeat("-", 55))
 
 	nums := []int{5, 3, 19, 12, 3}
-	
+
 	result := bubbleSort(nums)
 	for i := 0; i < len(result); i++ {
-		fmt.Printf("%d: %d\n", i + 1, result[i])
+		fmt.Printf("%d: %d\n", i+1, result[i])
 	}
-
 }
 
 func bubbleSort(nums []int) []int {
 
 	for i := 0; i < len(nums); i++ {
-		for j := 0; j < len(nums) - 1; j++ {
-			if nums[j] > nums[j + 1] {
-				nums[j], nums[j + 1] = nums[j + 1], nums[j]
+		for j := 0; j < len(nums)-1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
 			}
 		}
 	}
