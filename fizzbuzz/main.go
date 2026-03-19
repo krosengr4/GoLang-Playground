0d66500792c8a0915590ed191bbaa40549953afa
Kevin Rosengren <rosenkev4@gmail.com>
2026-03-19 15:32:09 -0500

    Go saving reformat

--- fizzbuzz/main.go
+++ fizzbuzz/main.go
@@ -11,11 +11,11 @@ import "fmt"
 func main() {
 	for i := 1; i <= 100; i++ {
 		switch {
-		case i % 15 == 0:
+		case i%15 == 0:
 			fmt.Println("fizzbuzz!")
-		case i % 5 == 0:
+		case i%5 == 0:
 			fmt.Println("buzz")
-		case i % 3 == 0:
+		case i%3 == 0:
 			fmt.Println("fizz")
 		default:
 			fmt.Println(i)
